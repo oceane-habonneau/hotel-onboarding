70981e6dfaf0a279c9ed67b40d04f93cd8b2e937
oceane-habonneau <oceane.habonneau@gmail.com>
2026-03-04 22:54:33 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hotel-onboarding/', // ← ton nom de repo exact
+  base: '/hotel-onboarding/', // ← nom exact de ton repo
 })
